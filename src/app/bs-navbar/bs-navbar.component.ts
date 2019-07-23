5376b2fbb20e5b969b5d802557a43912db29f379
Yogesh Koli <info@yogeshkoli.com>
2019-07-23 22:15:13 +0530

    Use Async pipe to solve subcribe issue with firebase

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Component, OnInit } from "@angular/core";
 import * as firebase from "firebase";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "bs-navbar",
@@ -8,12 +9,10 @@ import * as firebase from "firebase";
   styleUrls: ["./bs-navbar.component.scss"]
 })
 export class BsNavbarComponent implements OnInit {
-  user: firebase.User;
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => {
-      this.user = user;
-    });
+    this.user$ = afAuth.authState;
   }
 
   ngOnInit() {}
