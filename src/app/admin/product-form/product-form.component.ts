3c82fe3d0b44b1f590c595b6b0ae5ffe4508aacb
Yogesh Koli <info@yogeshkoli.com>
2019-07-24 21:21:04 +0530

    Product Card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
@@ -11,8 +12,9 @@ export class ProductFormComponent implements OnInit {
   categories$;
 
   constructor(
-    categoryService: CategoryService,
-    private productService: ProductService
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private router: Router
   ) {
     this.categories$ = categoryService.getCategories();
   }
@@ -21,5 +23,6 @@ export class ProductFormComponent implements OnInit {
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 }
