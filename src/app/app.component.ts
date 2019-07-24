8380cb26b1d4a7864e6aa1891d8926d403d1d4e9
Yogesh Koli <info@yogeshkoli.com>
2019-07-24 15:20:36 +0530

    Fix login redirect issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,16 @@ export class AppComponent {
     router: Router
   ) {
     auth.user$.subscribe(user => {
-      if (user) {
-        this.userService.save(user);
+      if (!user) return;
 
-        // let returnUrl = localStorage.getItem("returnUrl");
-        // router.navigateByUrl(returnUrl);
-      }
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+
+      if (!returnUrl) return;
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
     });
   }
 }
