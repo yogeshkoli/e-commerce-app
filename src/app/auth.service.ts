e4e828452b44c677813bde4ef007dba5843e0be5
Yogesh Koli <info@yogeshkoli.com>
2019-07-24 16:25:08 +0530

    Fix logout issue

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,7 @@ import { UserService } from './user.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import { auth } from 'firebase/app';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
 import { AppUser } from './models/app-user';
@@ -35,7 +35,11 @@ export class AuthService {
   get appUser$(): Observable<AppUser> {
     return this.user$.pipe(
       switchMap(user => {
-        return this.userService.get(user.uid);
+        if (user) {
+          return this.userService.get(user.uid);
+        }
+
+        return of(null);
       })
     );
   }
